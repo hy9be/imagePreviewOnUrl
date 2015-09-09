d6bacad33d96dcefd2f993cdfec22c6cc4d26005
Haowen You <hyou@microstrategy.com>
2015-09-09 01:31:22 -0400

    Finish

--- plugin/urlChecker.js
+++ plugin/urlChecker.js
@@ -4,17 +4,31 @@ if(!('contains' in String.prototype)) {
   };
 }
 
-$(".ember-view.m__activity-feed__event-metadata__item .t__light-text").each(function(index, element) {
-    if ($(this).text().startsWith("Tile info:")) {
-      // Get image url
-      $.each($(this).text().split('"'), function( i, val ) {
-        if (val.contains(".png") || val.contains(".jpg")) {
-          // Display the image
-          $(this).mouseenter(function() {
-            $(body).append( $("<div style='top: 0; left: 0; width: 240px; height: 240px; position: fixed;'><img style='margin: 10px; width: 220px; height: 220px;' src='"
-            + "http://pbs.twimg.com/media/" + val + "' /></div>") );
-          });
-        }
-      });
-    }
-});
+function preview() {
+  $(".ember-view.m__activity-feed__event-metadata__item .t__light-text").each(function(index, element) {
+      if ($(this).text().contains("Tile info:")) {
+        var textDiv = $(this)
+
+        // Get image url
+        $.each($(this).text().split('"'), function( i, val ) {
+          if (val.contains(".png") || val.contains(".jpg")) {
+            // Display the image
+            var segs = val.split(":"), imgUrl = "";
+
+            if (!val.startsWith("http")) {
+              imgUrl = "http://pbs.twimg.com/media/" + segs[0];
+            } else {
+              imgUrl = segs[0] + ":" + segs[1];
+            }
+
+            textDiv.mouseenter(function() {
+              $("body").append( $("<div style='background: #fff; top: 0; left: 0; width: 240px; height: 240px; position: fixed;'><img style='margin: 10px; width: 220px; height: 220px;' src='"
+               + imgUrl + "' /></div>") );
+            });
+          }
+        });
+      }
+  });
+}
+
+setTimeout(preview, 5000);
